85c06854ed9a0b132fdbe097c3bfdb8247d54fba
Tonmoy <rashid920129@gmail.com>
2023-02-07 10:53:08 +0000

    codes get_winner method

--- manual_rps.py
+++ manual_rps.py
@@ -5,7 +5,40 @@ def get_computer_choice():
     return computer_choice
 
 def get_user_choice(): 
-    user_choice = input("Enter your move ")
+    user_choice = input("Enter your choice ")
     return user_choice
 
+def get_winner(computer_choice, user_choice): 
+    winner = ""
+    if computer_choice == "Rock":
+        if user_choice == "Rock":
+            print("It is a tie!")
+            winner = "tie"
+        elif user_choice == "Paper":
+            print("You won!")
+            winner = "user"
+        else: 
+            print("You lost")
+            winner = "computer"
+    elif computer_choice == "Paper": 
+        if user_choice == "Rock":
+            print("You lost")
+            winner = computer
+        elif user_choice == "Paper":
+            print("It is a tie!")
+            winner = "tie"
+        else: 
+            print("You won!")
+            winner = "user"
+    else: 
+        if user_choice == "Rock":
+            print("You won!")
+            winner = "user"
+        elif user_choice == "Paper":
+            print("You lost")
+            winner = "computer"
+        else: 
+            print("It is a tie!")
+            winner = "tie"
 
+    return winner
